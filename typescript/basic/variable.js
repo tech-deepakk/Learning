5942f859d0a0abfd392a99ceff3c02ba83542d15
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-04-27 15:11:20 +0530

    variables in ts

---
+++ typescript/basic/variable.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var gretting = "Hello I am  deepak ";
+length = gretting.length;
+console.log(length);
+// number
+var myNum = 23;
+myNum = "rohan";
+console.log(myNum);
---
+++ typescript/basic/variable.ts
@@ -0,0 +1,19 @@
+let gretting: string = "Hello I am  deepak ";
+length = gretting.length;
+console.log(length);
+
+// 1.number
+const myNum = 23;
+
+// myNum = "rohan"; it will not allow in ts  it gives an error
+console.log(myNum);
+
+// 2.string
+let name: string;
+name = "deepakpanchal";
+
+// 3. boolean
+let isYouth: boolean;
+isYouth = true;
+
+export {};
