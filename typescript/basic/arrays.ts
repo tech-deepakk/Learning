f6b994584c809f086c5e7f6aa102c5fa5769675b
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-04-27 17:53:50 +0530

    array in ts

--- typescript/basic/arrays.ts
+++ typescript/basic/arrays.ts
@@ -1,17 +1,21 @@
+// 1. assign array type of string
 const superHeros: string[] = ["thor", "ironMan", "spiderman"];
+
+// 2. different method to type of array
 const heroPower: Array<number> = []; //another type to desc.. array
 superHeros.push("Wanda");
 superHeros.push("black Widow");
 
+// 3. object in an array
 type User = {
   name: string;
   email: string;
   isActive;
 };
-
 const allUser: User[] = [];
 allUser.push({ name: "deepak", email: "d@.com", isActive: true });
 
+//4. number of array  in an array
 const MlModel: number[][] = [
   [255, 255, 255],
   [252, 255, 255],
@@ -21,6 +25,7 @@ const MlModel: number[][] = [
   [255, 255, 113],
 ];
 
+// pop data from empty array
 const username: string[] = [];
 username.pop();
 
