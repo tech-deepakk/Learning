2de2d5478a06cdeea4fbd9ff8fdd376be1e29704
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-04-27 17:51:54 +0530

    array in ts

---
+++ typescript/basic/arrays.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = ["thor", "ironMan", "spiderman"];
+var heros = superHeros.concat();
+console.log(heros);
---
+++ typescript/basic/arrays.ts
@@ -0,0 +1,27 @@
+const superHeros: string[] = ["thor", "ironMan", "spiderman"];
+const heroPower: Array<number> = []; //another type to desc.. array
+superHeros.push("Wanda");
+superHeros.push("black Widow");
+
+type User = {
+  name: string;
+  email: string;
+  isActive;
+};
+
+const allUser: User[] = [];
+allUser.push({ name: "deepak", email: "d@.com", isActive: true });
+
+const MlModel: number[][] = [
+  [255, 255, 255],
+  [252, 255, 255],
+  [0, 0, 0],
+  [155, 255, 255],
+  [255, 213, 255],
+  [255, 255, 113],
+];
+
+const username: string[] = [];
+username.pop();
+
+export {};
