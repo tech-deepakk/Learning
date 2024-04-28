1be2de758be10b21422c96863fa22239b4254c5a
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-04-28 12:30:31 +0530

    enums in ts

--- typescript/basic/myEnums.js
+++ typescript/basic/myEnums.js
@@ -1,9 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// enums is special class that represent group of constants
 var seatChoice;
 (function (seatChoice) {
-  seatChoice[(seatChoice["AISLE"] = 0)] = "AISLE";
-  seatChoice[(seatChoice["MIDDLE"] = 1)] = "MIDDLE";
-  seatChoice[(seatChoice["WINDOW"] = 2)] = "WINDOW";
+    seatChoice[seatChoice["AISLE"] = 0] = "AISLE";
+    seatChoice[seatChoice["MIDDLE"] = 1] = "MIDDLE";
+    seatChoice[seatChoice["WINDOW"] = 2] = "WINDOW";
 })(seatChoice || (seatChoice = {}));
+// default value is start from 0
 var mySeat = seatChoice.AISLE;
+// single intialize
+var choice;
+(function (choice) {
+    choice[choice["A"] = 4] = "A";
+    choice[choice["B"] = 5] = "B";
+    choice[choice["C"] = 6] = "C";
+    choice[choice["D"] = 7] = "D";
+})(choice || (choice = {}));
+//  enums fully intialized string
+var direction;
+(function (direction) {
+    direction["NORTH"] = "NORTH";
+    direction["EAST"] = "EAST";
+    direction["WEST"] = "WEST";
+    direction["SOUTH"] = "SOUTH";
+})(direction || (direction = {}));
--- typescript/basic/myEnums.ts
+++ typescript/basic/myEnums.ts
@@ -1,8 +1,26 @@
+// enums is special class that represent group of constants
 enum seatChoice {
   AISLE,
   MIDDLE,
   WINDOW,
 }
 
+// default value is start from 0
 let mySeat = seatChoice.AISLE;
+
+// single intialize
+enum choice {
+  A = 4,
+  B,
+  C,
+  D,
+}
+
+//  enums fully intialized string
+enum direction {
+  NORTH = "NORTH",
+  EAST = "EAST",
+  WEST = "WEST",
+  SOUTH = "SOUTH",
+}
 export {};
